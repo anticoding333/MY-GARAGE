7dd19c211a045a95ec73db856c07f90bbe4a6609
LouaiMaayah <119330494+LouaiMaayah@users.noreply.github.com>
2023-12-16 15:19:08 +0100

    adsf

--- lib/students-page/StudentsPage.dart
+++ lib/students-page/StudentsPage.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:desktopapp/students-page/StudentsPageUtils.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as Path;
 
 class StudentsPage extends StatefulWidget {
   const StudentsPage({super.key});
@@ -98,4 +103,73 @@ class _StudentsPageState extends State<StudentsPage> {
       ]),
     );
   }
+}
+
+class DataBaseHelper {
+  DataBaseHelper._private();
+  static final DataBaseHelper instance = DataBaseHelper._private();
+
+  static Database? _database;
+  Future<Database> get database async => _database ?? await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = Path.join(documentsDirectory.path, 'CreditCard.db');
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''CREATE TABLE CreditCard(
+      id INTEGER PRIMARY KEY,
+      pin TEXT,
+      number TEXT
+    )''');
+  }
+
+  Future<List<CreditCard>> getCards() async {
+    Database db = await instance.database;
+    var CreditCards = await db.query('CreditCard', orderBy: 'id');
+    List<CreditCard> CreditCardList = CreditCards.isNotEmpty
+        ? CreditCards.map((e) => CreditCard.fromMap(e)).toList()
+        : [];
+
+    return CreditCardList;
+  }
+
+  Future<int> add(CreditCard card) async {
+    Database db = await instance.database;
+    return await db.insert("CreditCard", card.toMap());
+  }
+
+  Future<int> remove(int id) async {
+    Database db = await instance.database;
+    return await db.delete('CreditCard', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> update(CreditCard card) async {
+    Database db = await instance.database;
+    return await db.update("CreditCard", card.toMap(),
+        where: 'id = ?', whereArgs: [card.id]);
+  }
+
+  Future<CreditCard> getCard(CreditCard card) async {
+    Database db = await instance.database;
+    final c =
+        await db.query("CreditCard", where: 'id = ?', whereArgs: [card.id]);
+    return CreditCard.fromMap(c[0]);
+  }
+}
+
+class CreditCard {
+  int? id;
+  String? number;
+  String pin;
+
+  CreditCard({this.id, String? number, required this.pin}) {}
+
+  factory CreditCard.fromMap(Map<String, dynamic> json) {
+    return CreditCard(id: json["id"], number: json["number"], pin: json["pin"]);
+  }
+
+  Map<String, dynamic> toMap() => {"id": id, "number": number, "pin": pin};
 }
